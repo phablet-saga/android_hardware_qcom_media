c2c2de5f7a719d05e120d46657f3b0db573d8d6e
Giulio Cervera <giulio.cervera@gmail.com>
2012-02-23 19:27:47 +0100

    qcom/media: build only legacy audio
    mm-core and mm-video are 2 separate repos on ics.
    libstagefrighthw is built from hardware/msm7k and is keep
    there only for QComOMXMetadata.h inclusion.
    
    Change-Id: I0210bd41ac7c9f4c8969dca52f541af2f5759cdc
    
    Conflicts:
    
    	Android.mk
    	mm-core/Android.mk
    	mm-video/Android.mk

--- Android.mk
+++ Android.mk
@@ -7,8 +7,4 @@ ifeq ($(BOARD_USES_QCOM_HARDWARE),true)
 ifneq ($(BOARD_USES_AUDIO_LEGACY),true)
 	include $(QCOM_MEDIA_ROOT)/audio/Android.mk
 endif
-ifeq ($(BOARD_USES_QCOM_LIBS),true)
-	include $(QCOM_MEDIA_ROOT)/mm-core/Android.mk
-	include $(QCOM_MEDIA_ROOT)/mm-video/Android.mk
-endif
 endif
